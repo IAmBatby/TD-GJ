942018cc88ed2279aa6a21f6d181b67d26deaeae
IAmBatby <iambatby@gmail.com>
2024-12-12 20:28:04 +1100

    second fix

--- Assets/Project/Scripts/ScriptableAttributeWithDefaultValue.cs
+++ Assets/Project/Scripts/ScriptableAttributeWithDefaultValue.cs
@@ -4,26 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class ScriptableAttributeWithDefaultValue<T>
-{
-    [field: SerializeField] public ScriptableAttribute<T> Attribute { get; private set; }
-    [field: SerializeField] public T Value { get; private set; }
-
-    public void Instansiate()
-    {
-        Debug.Log("Instansiating : " + Attribute.name);
-        ScriptableAttribute<T> newAttribute = ScriptableAttribute<T>.Create(Attribute, Value);
-        Debug.Log("New Attribute Is: " + newAttribute.name);
-        Attribute = newAttribute;
-    }
-
-    public void ApplyWithReference(ScriptableAttributeWithDefaultValue<T> reference)
-    {
-        ScriptableAttribute<T> newAttribute = ScriptableAttribute<T>.Create(reference.Attribute, reference.Value);
-        Attribute = newAttribute;
-    }
-}
-[Serializable]
 public class ScriptableFloatAttributeWithDefaultValue
 {
     [field: SerializeField] public ScriptableFloatAttribute Attribute { get; private set; }
@@ -44,7 +24,22 @@ public class ScriptableFloatAttributeWithDefaultValue
     }
 }
 [Serializable]
-public class ScriptableIntAttributeWithDefaultValue : ScriptableAttributeWithDefaultValue<int>
+public class ScriptableIntAttributeWithDefaultValue
 {
+    [field: SerializeField] public ScriptableIntAttribute Attribute { get; private set; }
+    [field: SerializeField] public int Value { get; private set; }
 
+    public void Instansiate()
+    {
+        Debug.Log("Instansiating : " + Attribute.name);
+        ScriptableIntAttribute newAttribute = (ScriptableIntAttribute)ScriptableIntAttribute.Create(Attribute, Value);
+        Debug.Log("New Attribute Is: " + newAttribute.name);
+        Attribute = newAttribute;
+    }
+
+    public void ApplyWithReference(ScriptableIntAttributeWithDefaultValue reference)
+    {
+        ScriptableIntAttribute newAttribute = (ScriptableIntAttribute)ScriptableIntAttribute.Create(reference.Attribute, reference.Value);
+        Attribute = newAttribute;
+    }
 }
