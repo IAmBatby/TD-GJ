4ebc1154adc506c73a6267fc5028ff3e342bbcba
IAmBatby <iambatby@gmail.com>
2024-12-12 19:59:56 +1100

    fix?

--- Assets/Project/Scripts/ScriptableAttributeWithDefaultValue.cs
+++ Assets/Project/Scripts/ScriptableAttributeWithDefaultValue.cs
@@ -24,9 +24,24 @@ public class ScriptableAttributeWithDefaultValue<T>
     }
 }
 [Serializable]
-public class ScriptableFloatAttributeWithDefaultValue : ScriptableAttributeWithDefaultValue<float>
+public class ScriptableFloatAttributeWithDefaultValue
 {
+    [field: SerializeField] public ScriptableFloatAttribute Attribute { get; private set; }
+    [field: SerializeField] public float Value { get; private set; }
 
+    public void Instansiate()
+    {
+        Debug.Log("Instansiating : " + Attribute.name);
+        ScriptableFloatAttribute newAttribute = (ScriptableFloatAttribute)ScriptableFloatAttribute.Create(Attribute, Value);
+        Debug.Log("New Attribute Is: " + newAttribute.name);
+        Attribute = newAttribute;
+    }
+
+    public void ApplyWithReference(ScriptableFloatAttributeWithDefaultValue reference)
+    {
+        ScriptableFloatAttribute newAttribute = (ScriptableFloatAttribute)ScriptableFloatAttribute.Create(reference.Attribute, reference.Value);
+        Attribute = newAttribute;
+    }
 }
 [Serializable]
 public class ScriptableIntAttributeWithDefaultValue : ScriptableAttributeWithDefaultValue<int>
