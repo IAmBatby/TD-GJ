04925f37bb7d1f4017a4d2b92c8391c30b4fae8f
IAmBatby <iambatby@gmail.com>
2024-12-12 21:44:22 +1100

    Update ItemSpawner.cs

--- Assets/Project/Scripts/ItemSpawner.cs
+++ Assets/Project/Scripts/ItemSpawner.cs
@@ -11,13 +11,15 @@ public class ItemSpawner : MonoBehaviour
     private void Awake()
     {
         if (SpawnOnAwake)
-        {
-            ItemBehaviour newItem = Item.SpawnPrefab(transform);
-            if (Physics.Raycast(transform.position, new Vector3(transform.position.x, -5000, transform.position.z), out RaycastHit hit, Mathf.Infinity, dropMask))
-                newItem.transform.position = hit.point;
-            if (newItem.TryGetComponent(out IHittable hittable))
-                GameManager.Instance.AllSpawnedHittables.Add(hittable);
-        }
+            Spawn();
+    }
 
+    public void Spawn()
+    {
+        ItemBehaviour newItem = Item.SpawnPrefab(transform);
+        if (Physics.Raycast(transform.position, new Vector3(transform.position.x, -5000, transform.position.z), out RaycastHit hit, Mathf.Infinity, dropMask))
+            newItem.transform.position = hit.point;
+        if (newItem.TryGetComponent(out IHittable hittable))
+            GameManager.Instance.AllSpawnedHittables.Add(hittable);
     }
 }
