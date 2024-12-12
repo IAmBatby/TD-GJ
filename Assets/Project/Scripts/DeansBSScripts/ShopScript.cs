763c21d83f0e4c6ff0421540f0e406e45fcb3e44
DeanPenji <DeanSMalotsis@gmail.com>
2024-12-12 21:44:21 +1100

    umm

--- Assets/Project/Scripts/DeansBSScripts/ShopScript.cs
+++ Assets/Project/Scripts/DeansBSScripts/ShopScript.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ShopScript : MonoBehaviour
 {
+    public ItemSpawner spawner;
+    public ScriptableItem coin;
 
+    public void ToggleSpawnEatMoney(Collider other)
+    {
+        if (other.TryGetComponent(out ItemBehaviour item))
+            if (item.ItemData == coin) ;
+    }
 
 }
