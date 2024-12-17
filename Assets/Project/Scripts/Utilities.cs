dc9c7f6ee3f16eca2c8f459ac8baacbac4c415ce
IAmBatby <iambatby@gmail.com>
2024-12-17 22:05:46 +1100

    ContentSpawner Previews

--- Assets/Project/Scripts/Utilities.cs
+++ Assets/Project/Scripts/Utilities.cs
@@ -9,4 +9,35 @@ public static class Utilities
         //Debug.Log("Setting New Value to: " + (value + (waveCount * (scaleRate * value / 100))));
         return (value + (waveCount * (scaleRate * value / 100)));
     }
+
+    public static void DrawPrefabPreview(Transform context, GameObject prefab, Color primary, Color secondary, ref List<MeshFilter> filters)
+    {
+        Matrix4x4 prevMatrix = Gizmos.matrix;
+        Color prevColor = Gizmos.color;
+        Gizmos.matrix = context.localToWorldMatrix;
+
+        if (prefab == null) return;
+        if (filters == null)
+            filters = new List<MeshFilter>();
+        if (filters.Count == 0)
+            foreach (MeshFilter renderer in prefab.GetComponentsInChildren<MeshFilter>())
+                filters.Add(renderer);
+
+        for (int i = 0; i < filters.Count; i++)
+            if (filters[i] == null || filters[i].transform.root != prefab.transform)
+            {
+                filters.Clear();
+                return;
+            }
+
+        Gizmos.color = new Color(primary.r, primary.g, primary.b, 0.3f);
+        foreach (MeshFilter renderer in filters)
+            Gizmos.DrawMesh(renderer.sharedMesh, renderer.transform.position, renderer.transform.rotation, renderer.transform.lossyScale);
+        Gizmos.color = new Color(secondary.r, secondary.g, secondary.b, 0.05f);
+        foreach (MeshFilter renderer in filters)
+            Gizmos.DrawWireMesh(renderer.sharedMesh, renderer.transform.position, renderer.transform.rotation, renderer.transform.lossyScale);
+
+        Gizmos.color = prevColor;
+        Gizmos.matrix = prevMatrix;
+    }
 }
